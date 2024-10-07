9969a471d6ed4cee9fa2d8690786d24febc18052
jvu <momabz6@gmail.com>
2024-10-07 10:40:16 +0700

    me and the best way people works together now.

---
+++ Mon-7-Oct-2024.js
@@ -0,0 +1,13 @@
+// Mon 7, Oct 2024
+// I'm writing this from my parents house after years of doing some unproductive for my tech career in terms of finance and time aspects. Now I created this repo to document my job/tech carreers as serious as it is.
+// AI is taking over the news and Mark is about to manufactor the Quest in VN so all my writing in this repo is my knowing from the world (not AI-generated) so it's gonna be raw as new-borns.
+// I need to get back to live my life as a developer/engineer not an businessman. I know some business people, I met thousands of customers and I started acting like a saleman or customer support person, that doesn't help me here. It's not productive for the mind.
+
+// just fixed my comment in JS from ## -> // btw.
+
+// I like more about developers friends, they're more determine, hard-working, humorous, ...
+// I'm about to get a React developer job because that's the quickest way to move out right now. I won't mind if my hands are hurt anymore. Now the brain is more important, it's harder to fix. So I enjoys more the async issue solving of Github and the Git. It's the best way to work with different people. I did a help for my family sister to help her decorate her house, it's not a lot of works but it turned out a not-funny experience of working. I like working with funny people doing serious works.
+
+// I need to eat this Pho(Phở) from yesterday.
+
+//
